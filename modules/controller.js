5b9c9fa67a7b0e2846cb8595dfbef39064cc7057
Timothy Cole <tim@timcole.me>
2017-02-02 17:56:43 -0500

    Module command working change

--- modules/controller.js
+++ modules/controller.js
@@ -9,10 +9,10 @@ exports.load = (cfg, start) => {
 	config = cfg;
 	var loaded = 0;
 
-	for (var i = 0; i < config.modules.length; i++) {
-		if(typeof modules[config.modules[i].require] !== 'undefined') log.critical("You can't have two modules with the same name loaded!");
-		if(config.modules[i].require) {
-			modules[config.modules[i].require] = require(`./commands/${config.modules[i].require}.js`);
+	for (var i = 0; i < config.commands.length; i++) {
+		if(typeof modules[config.commands[i].module] !== 'undefined') log.critical("You can't have two modules with the same name loaded!");
+		if(config.commands[i].module) {
+			modules[config.commands[i].module] = require(`./commands/${config.commands[i].module}.js`);
 			loaded++;
 		}
 	}
@@ -23,12 +23,12 @@ exports.load = (cfg, start) => {
 }
 
 exports.check = (data) => {
-	for (var i = 0; i < config.modules.length; i++) {
-		if (data.message.toLowerCase() === config.modules[i].command.toLowerCase()) {
-			if(config.modules[i].require) modules[config.modules[i].require].trigger(data);
-			if(config.modules[i].response) {
-				data.client.say(dat	a.channel, config.modules[i].response);
-				log.pass(`${color.cyan(config.modules[i].command.toLowerCase())} was issued in ${color.cyan(data.channel)} by ${color.cyan(data.userstate['display-name'] || data.userstate.username)}`);
+	for (var i = 0; i < config.commands.length; i++) {
+		if (data.message.toLowerCase() === config.commands[i].command.toLowerCase()) {
+			if(config.commands[i].module) modules[config.commands[i].module].trigger(data);
+			if(config.commands[i].response) {
+				data.client.say(data.channel, config.commands[i].response);
+				log.pass(`${color.cyan(config.commands[i].command.toLowerCase())} was issued in ${color.cyan(data.channel)} by ${color.cyan(data.userstate['display-name'] || data.userstate.username)}`);
 			}
 		}
 	}
