fca040354778fcd74d4a3463dfb6365abaf7cbe5
Timothy Cole <tim@timcole.me>
2017-02-03 02:18:04 -0500

    debug

--- modules/commands/debug.js
+++ modules/commands/debug.js
@@ -1,7 +1,10 @@
 const log = require('../log.js');
+const color = require("colors/safe");
 
 var exports = module.exports = {};
 
 exports.trigger = (data) => {
-	console.log(data);
+    var user = data.userstate['display-name'] || data.userstate.username;
+	data.client.say(data.channel, `Hey there @${user}!`);
+	log.pass(`Said ${color.cyan("hello")} to ${color.cyan(user)}.`);
 };
