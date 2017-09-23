4252a513063a013b0ac939ac4084bf0a18b3c8dc
Timothy Cole <tim@timcole.me>
2017-09-23 03:38:21 -0400

    Forgot one lol

--- app.js
+++ app.js
@@ -6,36 +6,36 @@ const controller = require("./modules/controller.js");
 
 controller.load(config, () => {
 	const options = {
-	    options: {
-	        debug: false
-	    },
-	    connection: {
-	        reconnect: true
-	    },
-	    identity: {
-	        username: config.credentials.username,
-	        password: config.credentials.oauth
-	    },
-	    channels: config.channels
+		options: {
+			debug: false
+		},
+		connection: {
+			reconnect: true
+		},
+		identity: {
+			username: config.credentials.username,
+			password: config.credentials.oauth
+		},
+		channels: config.channels
 	};
 
 	var client = new tmi.client(options);
 
 	client.on("connecting", () => {
-	    log.warn(`Connecting to ${color.magenta("Twitch")} as ${color.cyan(config.credentials.username)}.`);
+		log.warn(`Connecting to ${color.magenta("Twitch")} as ${color.cyan(config.credentials.username)}.`);
 	});
 
 	client.on("connected", () => {
-	    log.pass(`Connected to ${color.magenta("Twitch")}.`);
-	    log.pass(`Joined ${color.cyan(config.channels.join(", "))}.`);
+		log.pass(`Connected to ${color.magenta("Twitch")}.`);
+		log.pass(`Joined ${color.cyan(config.channels.join(", "))}.`);
 	});
 
 	client.on("disconnected", () => {
-	    log.error(`Disconnected from ${color.magenta("Twitch")}.`);
+		log.error(`Disconnected from ${color.magenta("Twitch")}.`);
 	});
 
 	client.on("chat", (channel, userstate, message, self) => {
-	    if (self) return;
+		if (self) return;
 		controller.check({client, channel, userstate, message});
 	});
 
