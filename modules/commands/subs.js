4a61d105f9c5351def0dbdcff434edec93c52da3
Timothy Cole <tim@timcole.me>
2017-11-26 09:31:05 -0500

    Don't need time conversion in here bud.

--- modules/commands/subs.js
+++ modules/commands/subs.js
@@ -22,21 +22,3 @@ exports.trigger = (data) => {
 
 	log.pass(`${color.cyan(data.message.toLowerCase())} was issued in ${color.cyan(data.channel)} by ${color.cyan(data.userstate.name)}`);
 };
-
-function dhm(t) {
-	var cd = 24 * 60 * 60 * 1000,
-		ch = 60 * 60 * 1000,
-		d = Math.floor(t / cd),
-		h = Math.floor((t - d * cd) / ch),
-		m = Math.round((t - d * cd - h * ch) / 60000);
-	if (m === 60) {
-		h++;
-		m = 0;
-	}
-	if (h === 24) {
-		d++;
-		h = 0;
-	}
-	// return [d, pad(h), pad(m)].join(':');
-	return `${ d } days ${ h } hour ${ m } minutes`;
-}
